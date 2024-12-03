2a4bfeed031551155258b3660e1a32932c2c33ed
johnmart19 <johnivan19999@gmail.com>
2024-12-03 23:46:47 +0200

    Revert "sm8250-common: Link libinput_shim to fix elf check for WFD blob"
    This reverts commit 08f134067eacf33c0206af1028b04581dee6f252.

--- extract-files.sh
+++ extract-files.sh
@@ -74,15 +74,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
             ;;
-        system_ext/lib64/libwfdnative.so)
-            [ "$2" = "" ] && return 0
-            grep -q "libbinder_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbinder_shim.so" "${2}"
-            grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
-            ;;
-        system_ext/lib64/libwfdservice.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V4-cpp.so" "${2}"
-            ;;
         vendor/etc/init/init.mi_thermald.rc)
             [ "$2" = "" ] && return 0
             sed -i "/seclabel u:r:mi_thermald:s0/d" "${2}"
